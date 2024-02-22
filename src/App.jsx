90aeac38375dd41bb91b4d9f98dc6d21f7b630e9
Aditya Chaurasia <126148159+Cat-The-Developer@users.noreply.github.com>
2024-02-22 14:16:04 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { Input, Navbar } from "./components";
 
 const App = () => {
   return (
-    <div className="py-[5%] px-[7%] main-grad">
+    <div className="sm:py-[5%] sm:px-[7%] main-grad">
       <section className="flex flex-col p-9 border rounded-xl shadow-xl bg-white h-screen">
         <Navbar />
         <Input />
